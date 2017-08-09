29287d5616d7b5023ed43708580455ee6daca194
Babasanya <babscraig@gmail.com>
2017-08-09 23:57:04 +0100

    Add login flow

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Card, CardSection, Input, MyButton } from './common';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 
 
 class LoginForm extends Component {
   onEmailChange(text) {
     this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.onPasswordChange(text);
   }
 
   render() {
@@ -15,6 +19,7 @@ class LoginForm extends Component {
         <CardSection>
           <Input
             label="Email"
+            value={this.props.email}
             onChangeText={this.onEmailChange.bind(this)}
             placeholder="email@gmail.com" />
         </CardSection>
@@ -23,6 +28,7 @@ class LoginForm extends Component {
           <Input
             secureTextEntry
             label="Password"
+            onChangeText={this.onPasswordChange.bind(this)}
             placeholder="Password" />
         </CardSection>
 
@@ -36,4 +42,12 @@ class LoginForm extends Component {
   }
 }
 
-export default connect(null, {emailChanged})(LoginForm);
+const mapStateToProps = (state) => {
+  const { email, password } = state.auth
+  return {
+    email: email,
+    password: password
+  }
+};
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
