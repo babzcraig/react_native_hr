a4e9fd72be17a2c1f53a2dba9e4d7c2a738a0b66
Babs Craig <babscraig@gmail.com>
2017-08-12 00:11:45 +0100

    Refactored Actions structure

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,18 @@
+import { 
+    EMPLOYEE_UPDATE
+} from '../actions/types';
+
+const INITIAL_STATE = { name: '', phone: '', shift: '' }
+
+
+export default(state = INITIAL_STATE, action) => {
+
+    console.log(action);
+    switch(action.type) {
+        case EMPLOYEE_UPDATE:
+            // action.payload === { prop: 'name', value: 'Jane'}
+            return { ...state, [action.payload.prop]: action.payload.value }
+        default:
+            return state;
+    }
+};
