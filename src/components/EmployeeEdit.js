dcba5c6ecc53a7e195bd1f1f1708a2852efb2343
Babs Craig <babscraig@gmail.com>
2017-08-15 21:26:40 +0100

    Add Confirm component

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -5,6 +5,7 @@ import { Text } from 'react-native';
 import { employeeUpdate, employeeEdit } from '../actions';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import Communications from 'react-native-communications';
 
 class EmployeeEdit extends Component {
     componentDidMount() {
@@ -18,6 +19,11 @@ class EmployeeEdit extends Component {
         this.props.employeeEdit({ name, shift, phone, uid: this.props.employee.uid });
     }
 
+    onTextPress = () => {
+        const { phone, shift } = this.props;
+        Communications.text(phone, `Your upcoming shift is on ${shift}`);
+    }
+
     render() {
         return (
             <Card>
@@ -25,6 +31,12 @@ class EmployeeEdit extends Component {
                 <CardSection>
                     <MyButton onPress={this.onButtonPress}>
                         Save Changes
+                    </MyButton>
+                </CardSection>
+
+                <CardSection>
+                    <MyButton onPress={this.onTextPress}>
+                        Text Schedule
                     </MyButton>
                 </CardSection>
             </Card>
---
+++ src/components/common/Confirm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, Modal } from 'react-native';
+import { CardSection } from './CardSection';
+import { MyButton } from './MyButton';
+
+const Confirm = () => {
+
+}
+
+export { Confirm };
